c503645ef42f483b77aa7c90e00c2bf45e48583a
Cotze <carlosrodigoatl@gmail.com>
2022-01-13 18:55:15 -0600

    Projecto base

---
+++ Modelar-gerente/Gerente.cs
@@ -0,0 +1,45 @@
+﻿using Modelar_gerente;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelar_gerente
+{
+    public class Gerente
+    {
+        Persona persona = new Persona();
+        
+        string nombre;
+
+        public string Nombre { get => Nombre; set => Nombre = value; }
+
+
+        public Gerente()
+        {
+            
+        }
+
+        public Gerente(String nom)
+        {
+            this.nombre = nom;
+        }
+        /// <summary>
+        /// Metodo para escribir el nombre del gerente solicitado por el usuario e imprimido en consola
+        /// </summary>
+        public void mostrarDatos()
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine("Escribir el nombre del gerente que te entrevistara: ");
+            this.nombre = Console.ReadLine();
+            Console.WriteLine("");
+         
+            Console.WriteLine($"{nombre} esta entrevistando a {persona.Nombre}" );
+
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine("Favor de revisar la plataforma mas tarde");
+            
+        }
+    }
+}
---
+++ Modelar-gerente/Persona.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelar_gerente
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+
+        /// <summary>
+        /// Variable asignada para el uso en la clse gerente
+        /// </summary>
+        public Persona()
+        {
+            Nombre = "Carlos";
+        }
+    }
+}
